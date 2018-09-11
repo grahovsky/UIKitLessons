51fa3039746564860f3f2849a5522e355f892362
Konstantin <grahovsky@gmail.com>
2018-09-12 00:26:07 +0300

    ui_button

--- UIKit_lessons/ViewController.swift
+++ UIKit_lessons/ViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet var actionButtons: [UIButton]!
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        label.isHidden = true
+        label.font = label.font.withSize(35)
+        
+        button.isHidden = true
+        
+        for button in actionButtons{
+            button.setTitleColor(.blue, for: .normal)
+            button.backgroundColor = .green
+        }
+        
+        button.setTitle("Clear", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .red
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +38,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func pressedButton(_ sender: UIButton) {
+        
+        label.isHidden = false
+        button.isHidden = false
+        
+        if sender.tag == 0 {
+            label.text = "Hi, man"
+            label.textColor = .red
+            
+        } else if sender.tag == 1 {
+            label.text = "Hi there"
+            label.textColor = .blue
+            
+        } else if sender.tag == 2 {
+            label.isHidden = true
+            button.isHidden = true
+        }
 
+    }
+    
 }
 
