f7c271be128db2a8948a44891d577e62232eb98e
Konstantin <grahovsky@gmail.com>
2018-09-14 12:50:03 +0300

    20180914

--- UIKit_lessons/ViewController.swift
+++ UIKit_lessons/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segmentedContol: UISegmentedControl!
+    
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        label.isHidden = true
+        label.font = label.font.withSize(35)
+        label.textAlignment = .center
+        label.numberOfLines = 2
+        
+        segmentedContol.insertSegment(withTitle: "Third", at: 2, animated: true)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func choiceSegment(_ sender: Any) {
+        
+        label.isHidden = false
+        
+        switch segmentedContol.selectedSegmentIndex {
+        case 0:
+            label.text = "The first segment is selected"
+            label.textColor = .red
+        case 1:
+            label.text = "The second segment is selected"
+            label.textColor = .green
+        case 2:
+            label.text = "The third segment is selected"
+            label.textColor = .yellow
+        default:
+            print("Sothing wrong")
+        }
+        
+        
+    }
+    
 }
 
