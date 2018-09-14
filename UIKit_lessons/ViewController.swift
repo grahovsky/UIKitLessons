95ec0ed25b88e08d538de0430b055ac40adf69a2
Konstantin <grahovsky@gmail.com>
2018-09-14 14:43:52 +0300

    20180914

--- UIKit_lessons/ViewController.swift
+++ UIKit_lessons/ViewController.swift
@@ -14,15 +14,27 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var label: UILabel!
     
+    @IBOutlet weak var slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
        
-        label.isHidden = true
+        slider.minimumValue = 0
+        slider.maximumValue = 100
+        slider.value = 50;
+        sliderAction(slider)
+        
+        label.text = String(slider.value)
+        //label.isHidden = true
         label.font = label.font.withSize(35)
         label.textAlignment = .center
         label.numberOfLines = 2
         
         segmentedContol.insertSegment(withTitle: "Third", at: 2, animated: true)
+        
+        slider.minimumTrackTintColor = .white
+        slider.maximumTrackTintColor = .black
+        slider.thumbTintColor = .black
         
     }
 
@@ -51,6 +63,14 @@ class ViewController: UIViewController {
         
         
     }
+    
+    @IBAction func sliderAction(_ sender: UISlider) {
+        label.text = String(sender.value)
+        
+        let backgroundColor = self.view.backgroundColor
+        self.view.backgroundColor = backgroundColor?.withAlphaComponent(CGFloat(sender.value/100))
+    }
+    
     
 }
 
