83356a3ec373091dc39468d37211c6ba2896c0c7
Konstantin <grahovsky@gmail.com>
2018-09-18 10:20:55 +0300

    #UICollectionViewCell

---
+++ UIKit_lessons/ArtCoverCell.swift
@@ -0,0 +1,15 @@
+//
+//  ArtCoverCell.swift
+//  UIKit_lessons
+//
+//  Created by Konstantin on 18.09.2018.
+//  Copyright © 2018 Konstantin. All rights reserved.
+//
+
+import UIKit
+
+class ArtCoverCell: UICollectionViewCell {
+    
+    @IBOutlet weak var coverImageView: UIImageView!
+    
+}
