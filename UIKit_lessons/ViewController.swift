231f61899a0491627f341d8d651e0fafd8979650
Konstantin <grahovsky@gmail.com>
2018-09-20 21:24:24 +0300

    #WKWebView #delegate #info.plist

--- UIKit_lessons/ViewController.swift
+++ UIKit_lessons/ViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webWiew: WKWebView!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var forwardButton: UIButton!
+    @IBOutlet weak var urlTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        urlTextField.delegate = self
+        webWiew.navigationDelegate = self
+        
+        let homePage = "https://www.apple.com"
+        let url = URL(string: homePage)
+        let request = URLRequest(url: url!)
+        
+        urlTextField.text = homePage
+        
+        webWiew.load(request)
+        webWiew.allowsBackForwardNavigationGestures = true
+        
+        // для http нужно зайти в info.plist и добавить "App Transport Security Settings" -> "Allow Arbitrary Loads" -> "YES"
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +39,46 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func backButtonAction(_ sender: Any) {
+        
+        if webWiew.canGoBack {
+            webWiew.goBack()
+        }
+        
+    }
+    
+    @IBAction func forwardButtonAction(_ sender: Any) {
+        
+        if webWiew.canGoForward {
+            webWiew.goForward()
+        }
+        
+    }
+    
 }
 
+extension ViewController: UITextFieldDelegate, WKNavigationDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    
+        let urlString = textField.text!
+        let url = URL(string: urlString)!
+        let request = URLRequest(url: url)
+        
+        webWiew.load(request)
+        
+        textField.resignFirstResponder()
+        
+        return true
+        
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        
+        urlTextField.text = webView.url?.absoluteString
+        
+        backButton.isEnabled = webView.canGoBack
+        forwardButton.isEnabled = webView.canGoForward
+        
+    }
+}
