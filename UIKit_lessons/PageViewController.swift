09568e2535bcfbb61806cabbe972c37dbc5e00c7
Konstantin <grahovsky@gmail.com>
2018-09-20 23:55:59 +0300

    #extension #UIPageViewControllerDataSource #dataSource #dismis

--- UIKit_lessons/PageViewController.swift
+++ UIKit_lessons/PageViewController.swift
@@ -14,13 +14,16 @@ class PageViewController: UIPageViewController {
     "Первая глава. Как я установил икс код. Далее идет какой-то странный текст",
     "Вторая глава. Как я изучал икс код. Далее идет какой-то странный текст",
     "Третяя глава. Как я использовал икс код. Далее идет какой-то странный текст",
-    "Последняя глава. Как я разукрасил икс код. Далее идет какой-то странный текст"
+    "Последняя глава. Как я разукрасил икс код. Далее идет какой-то странный текст",
+    ""
     ]
     
-    let emojiArray = ["👿", "👤", "👲", "🧐"]
+    let emojiArray = ["👿", "👤", "👲", "🧐", ""]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        dataSource = self
         
         if let contentViewConroller = showViewControllerAtIndex(0) {
             setViewControllers([contentViewConroller], direction: .forward, animated: true, completion: nil)
@@ -31,7 +34,10 @@ class PageViewController: UIPageViewController {
     func showViewControllerAtIndex(_ index: Int) -> ContentViewController? {
         
         guard index >= 0 else { return nil }
-        guard index < presentScreenContent.count else { return nil }
+        guard index < presentScreenContent.count else {
+            dismiss(animated: true, completion: nil)
+            return nil
+        }
         //тут создаем экземпляр через поиск по Storyboard ID
         guard let contetViewController = storyboard?.instantiateViewController(withIdentifier: "ContentViewController") as? ContentViewController else { return nil }
         
@@ -43,5 +49,25 @@ class PageViewController: UIPageViewController {
         return contetViewController
         
     }
+    
+}
+
+extension PageViewController: UIPageViewControllerDataSource {
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        var pageNumber = (viewController as! ContentViewController).currentPage
+        pageNumber -= 1
+        
+        return showViewControllerAtIndex(pageNumber)
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        var pageNumber = (viewController as! ContentViewController).currentPage
+        pageNumber += 1
+        
+        return showViewControllerAtIndex(pageNumber)
+    }
+    
     
 }
